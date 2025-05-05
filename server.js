282d0289e7483b10a4f7aef9d71a8860f826c580
vaishali4566 <vaishaliverma@globussoft.in>
2025-05-05 18:32:58 +0530

    added cors

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users/user.route.js';
 import connectDB from './DB/db.config.js';
+import cors from 'cors';
 const app = express();
 dotenv.config();
 connectDB(); 
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin : "http://localhost:5173",
+    methods : "GET, POST, PUT, DELETE, HEAD",
+    credentials : true
+}
 
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
